6ac53cb3dd59e5e972e6ef4c3e0343650d9ee3e7
patriciamsro <patriciamsro@gmail.com>
2022-05-15 14:19:25 -0300

    Adjust Github link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,10 +7,10 @@ function Navbar() {
         <nav className="container">
           <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/devsuperior">
+            <a href="https://github.com/patriciamsro/dsmovie/commits?author=patriciamsro">
               <div className="dsmovie-contact-container">
                 <GithubIcon/>
-                <p className="dsmovie-contact-link">/devsuperior</p>
+                <p className="dsmovie-contact-link">/patriciamsro</p>
               </div>
             </a>
           </div>
